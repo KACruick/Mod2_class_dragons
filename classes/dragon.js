2a01329d62ebf2be37ba29b096caeefd64d55297
kyle <averytkyle@gmail.com>
2024-04-27 12:20:22 -0400

    Phase 1

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,4 +1,19 @@
-// Your code here 
+class Dragon {
+  constructor(name, color) {
+    this.name = name
+    this.color = color
+  }
+
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`
+  }
+
+  static getDragons(...dragons) {
+    let dragonArr = dragons.map((dragon) => dragon.name)
+    return dragonArr
+  }
+ 
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
